cef6f8a26bdaef02069c1c00015e0131a6983722
salman masood <salmanmasood917@gmail.com>
2024-01-20 08:06:23 +0530

    update nextjs config file (styledcomponents)

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'cdn.sanity.io',
-            port: '',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
       },
-}
+    ],
+  },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
